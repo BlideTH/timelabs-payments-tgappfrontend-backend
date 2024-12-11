c6394a59c7aca286732f863cf57f1b3be38594b5
Leo <blidetfh@yandex.ru>
2024-12-11 16:06:53 +0300

    modified:   src/environments/environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
-  apiUrl: 'https://us-central1-timelabs-payments-backen-6c183.cloudfunctions.net',
+  apiUrl: '',
   paymentTokens: {
-    payMaster: '1744374395:TEST:10c29da1f872a094090d',
-    yuKassa: '381764678:TEST:103259',
-    sberbank: '401643678:TEST:dabddf45-c30e-44f0-b21c-aa5a3c376747',
+    payMaster: '',
+    yuKassa: '',
+    sberbank: '',
   },
   firebaseConfig: {
-    apiKey: 'AIzaSyAYVjefjB6NFRuH0b5J4dx7QUPyXZLB1gE',
-    authDomain: 'timelabs-payments-app.firebaseapp.com',
-    projectId: 'timelabs-payments-app',
-    storageBucket: 'timelabs-payments-app.firebasestorage.app',
-    messagingSenderId: '36341459005',
-    appId: '1:36341459005:web:c655b113d69cfb168ae254',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
   },
 };

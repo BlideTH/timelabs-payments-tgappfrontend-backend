fe0180314aa9365b189fc08abe3e4615d115ca33
BlideTH <33828790+BlideTH@users.noreply.github.com>
2024-12-16 17:17:21 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
-  apiUrl: 'https://us-central1-timelabs-payments-backen-6c183.cloudfunctions.net',
+  apiUrl: '',
   paymentTokens: {
-    payMaster: '1744374395:TEST:10c29da1f872a094090d',
-    yuKassa: '381764678:TEST:103259',
-    sberbank: '401643678:TEST:dabddf45-c30e-44f0-b21c-aa5a3c376747',
+    payMaster: '',
+    yuKassa: '',
+    sberbank: '',
   },
   firebaseConfig: {
-    apiKey: "AIzaSyAUZyCsx9y5HNX9CkNA_jx3yv_ZzAC55ac",
-    authDomain: "timelabs-payments-backen-6c183.firebaseapp.com",
-    projectId: "timelabs-payments-backen-6c183",
-    storageBucket: "timelabs-payments-backen-6c183.firebasestorage.app",
-    messagingSenderId: "923200619697",
-    appId: "1:923200619697:web:0260acd25a1a67d505ce90"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   },
 };
